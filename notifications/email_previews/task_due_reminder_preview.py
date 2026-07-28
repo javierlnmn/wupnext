f76b3ed71aa52a353f321bcaa378535097358e4a
javierlnmn <jlm03123@gmail.com>
2026-07-28 10:22:33 +0200

    Remove factory boy from task due reminder preview to allow prod

--- notifications/email_previews/task_due_reminder_preview.py
+++ notifications/email_previews/task_due_reminder_preview.py
@@ -1,10 +1,10 @@
 from datetime import timedelta
 
+from django.contrib.auth import get_user_model
 from django.utils import timezone
 
-from accounts.tests.factories import UserFactory
+from tasks.models import Group, Task
 from tasks.notifications.due_reminders import DueReminderNotification
-from tasks.tests.factories import GroupFactory, TaskFactory
 
 from ..models import NotificationEvent
 from .base import PREVIEW_EMAIL, PREVIEW_USERNAME, BaseEmailPreview
@@ -16,15 +16,35 @@ class TaskDueReminderPreview(BaseEmailPreview):
 
     def _seed(self):
         today = timezone.localdate()
-        self.user = UserFactory(username=PREVIEW_USERNAME, email=PREVIEW_EMAIL)
-        group = GroupFactory(user=self.user)
 
-        overdue = TaskFactory(
-            user=self.user, due_date=today - timedelta(days=2), weight=4, group=group
+        self.user = get_user_model()(
+            username=PREVIEW_USERNAME,
+            email=PREVIEW_EMAIL,
         )
-        TaskFactory(user=self.user, parent=overdue, completed=True)
-        TaskFactory(user=self.user, parent=overdue)
-        TaskFactory(user=self.user, due_date=today)
+        self.user.set_unusable_password()
+        self.user.save()
+
+        group = Group.objects.create(user=self.user, name="Side project")
+
+        overdue = Task.objects.create(
+            user=self.user,
+            name="Renew the domain",
+            due_date=today - timedelta(days=2),
+            weight=4,
+            group=group,
+        )
+        Task.objects.create(
+            user=self.user,
+            name="Compare registrars",
+            parent=overdue,
+            completed_at=timezone.now(),
+        )
+        Task.objects.create(
+            user=self.user, name="Update the DNS records", parent=overdue
+        )
+        Task.objects.create(
+            user=self.user, name="Reply to the tax email", due_date=today
+        )
 
     def _get_notification_context(self):
         return {**DueReminderNotification().context(self.user), "user": self.user}
