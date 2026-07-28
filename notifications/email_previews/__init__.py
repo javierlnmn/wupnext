dcb4ebbe7e42e37eb18421a08dbb7af847089015
javierlnmn <jlm03123@gmail.com>
2026-07-28 13:09:26 +0200

    Separate notifications registry

--- notifications/email_previews/__init__.py
+++ notifications/email_previews/__init__.py
@@ -1,14 +1,5 @@
-from django.utils.module_loading import import_string
-
-from ..exceptions import MissingPreview
-from ..models import NotificationEvent
 from .base import PREVIEW_EMAIL, PREVIEW_USERNAME, BaseEmailPreview
-
-PREVIEWS = {
-    NotificationEvent.TASK_DUE_REMINDER.value: (
-        "notifications.email_previews.task_due_reminder_preview.TaskDueReminderPreview"
-    ),
-}
+from .registry import PREVIEWS, get_preview
 
 __all__ = [
     "PREVIEWS",
@@ -17,13 +8,3 @@ __all__ = [
     "BaseEmailPreview",
     "get_preview",
 ]
-
-
-def get_preview(event):
-    path = PREVIEWS.get(event)
-    if path is None:
-        raise MissingPreview(
-            f"No preview registered for '{event}'. "
-            f"Available: {', '.join(sorted(PREVIEWS)) or 'none'}."
-        )
-    return import_string(path)()
---
+++ notifications/email_previews/registry.py
@@ -0,0 +1,16 @@
+from ..exceptions import MissingPreview
+from .task_due_reminder_preview import TaskDueReminderPreview
+
+PREVIEWS = {
+    TaskDueReminderPreview.event: TaskDueReminderPreview,
+}
+
+
+def get_preview(event):
+    preview_class = PREVIEWS.get(event)
+    if preview_class is None:
+        raise MissingPreview(
+            f"No preview registered for '{event}'. "
+            f"Available: {', '.join(sorted(PREVIEWS)) or 'none'}."
+        )
+    return preview_class()
