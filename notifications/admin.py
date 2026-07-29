f9d47f5a719c4e5afbeef2dd635c512e86bfbdff
javierlnmn <jlm03123@gmail.com>
2026-07-29 14:57:35 +0200

    Create new notifications models admin

--- notifications/admin.py
+++ notifications/admin.py
@@ -1,6 +1,38 @@
 from django.contrib import admin
 
-from .models import NotificationLog
+from .models import (
+    NotificationChannelSwitch,
+    NotificationEventSwitch,
+    NotificationLog,
+    NotificationUserPreference,
+)
+
+
+class SwitchAdmin(admin.ModelAdmin):
+    def has_add_permission(self, request):
+        return False
+
+
+@admin.register(NotificationChannelSwitch)
+class NotificationChannelSwitchAdmin(SwitchAdmin):
+    list_display = ('channel', 'enabled')
+    list_editable = ('enabled',)
+
+
+@admin.register(NotificationEventSwitch)
+class NotificationEventSwitchAdmin(SwitchAdmin):
+    list_display = ('event', 'channel', 'enabled', 'on_by_default')
+    list_editable = ('enabled', 'on_by_default')
+    list_filter = ('channel', 'enabled')
+
+
+@admin.register(NotificationUserPreference)
+class NotificationUserPreferenceAdmin(admin.ModelAdmin):
+    list_display = ('user', 'event', 'channel', 'enabled', 'updated_at')
+    list_filter = ('event', 'channel', 'enabled')
+    search_fields = ('user__username', 'user__email')
+    list_select_related = ('user',)
+    readonly_fields = ('updated_at',)
 
 
 @admin.register(NotificationLog)
